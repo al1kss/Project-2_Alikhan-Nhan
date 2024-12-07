e5b58e90a855f711ef06d4b4d6661fd45b2506f7
minh nhan nguyen <82298632+bun781@users.noreply.github.com>
2024-12-07 14:05:28 +0900

    Update gen_polynomial_graph.py

--- code/gen_polynomial_graph.py
+++ code/gen_polynomial_graph.py
@@ -1,16 +1,11 @@
 def gen_polynomial_graph(x: list, y: list, deg: int, data_smoothness: int) -> tuple:
     y_smoothed = []
-    for i in range(data_smoothness // 2):
-        y_section = y[:i + 1]
-        y_section_average = sum(y_section) / len(y_section)
-        y_smoothed.append(y_section_average)
-
-    for i in range(data_smoothness // 2, len(y) - data_smoothness//2):
+    for i in range(0, len(y) - data_smoothness):
         y_section = y[i:i + data_smoothness]
         y_section_average = sum(y_section) / data_smoothness
         y_smoothed.append(y_section_average)
 
-    end_section_start = len(y) - data_smoothness // 2 - 1 if data_smoothness % 2 != 0 else len(y) - data_smoothness // 2
+    end_section_start = len(y) - data_smoothness - 1 if data_smoothness % 2 != 0 else len(y) - data_smoothness
     for i in range(end_section_start, len(y)):
         y_section = y[i:]
         y_section_average = sum(y_section) / len(y_section)
