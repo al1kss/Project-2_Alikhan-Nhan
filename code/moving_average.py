af45093c6a2156d57b50cd38aa6cf5d144d3764f
minh nhan nguyen <82298632+bun781@users.noreply.github.com>
2024-12-07 12:53:32 +0900

    Update moving_average.py

--- code/moving_average.py
+++ code/moving_average.py
@@ -1,7 +1,16 @@
-def moving_average(windowSize: int, x: list) -> list:
-    x_smoothed = []
-    for i in range(0, len(x)-1-windowSize):
-        x_section = x[i:i+windowSize]
-        x_average = sum(x_section) / windowSize
-        x_smoothed.append(x_average)
-    return x_smoothed
+def moving_average(data_smoothness:int):
+    y_smoothed = []
+    for i in range(data_smoothness // 2):
+        y_section = y[:i + 1]
+        y_section_average = sum(y_section) / len(y_section)
+        y_smoothed.append(y_section_average)
+
+    for i in range(len(y) - data_smoothness):
+        y_section = y[i:i + data_smoothness]
+        y_section_average = sum(y_section) / data_smoothness
+        y_smoothed.append(y_section_average)
+
+    for i in range(len(y) - data_smoothness // 2, len(y)):
+        y_section = y[i:]
+        y_section_average = sum(y_section) / len(y_section)
+        y_smoothed.append(y_section_average)
